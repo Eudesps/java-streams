e7d2857a8ea635b94e2fd88080d61d514654308e
José Eudes <joseeudes969@gmail.com>
2025-12-08 17:13:08 -0300

    CommandLineRunner

--- estudos-java-streams/src/main/java/com/eudes/estudos_java_streams/EstudosJavaStreamsApplication.java
+++ estudos-java-streams/src/main/java/com/eudes/estudos_java_streams/EstudosJavaStreamsApplication.java
@@ -1,13 +1,18 @@
 package com.eudes.estudos_java_streams;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EstudosJavaStreamsApplication {
+public class EstudosJavaStreamsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudosJavaStreamsApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
