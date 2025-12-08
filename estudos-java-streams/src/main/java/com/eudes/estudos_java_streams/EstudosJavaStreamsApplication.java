b85f10a286b76d1d38da4d5d7cb649cc67224c92
José Eudes <joseeudes969@gmail.com>
2025-12-08 17:09:06 -0300

    primeiro commit

---
+++ estudos-java-streams/src/main/java/com/eudes/estudos_java_streams/EstudosJavaStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.eudes.estudos_java_streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosJavaStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosJavaStreamsApplication.class, args);
+	}
+
+}
