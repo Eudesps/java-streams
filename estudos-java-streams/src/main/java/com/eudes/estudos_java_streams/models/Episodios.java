522fde3daf8b1ac48785c3b461cbb2f0aac6d25b
José Eudes <joseeudes969@gmail.com>
2025-12-08 17:23:29 -0300

    json e classe episodios com atributos

--- estudos-java-streams/src/main/java/com/eudes/estudos_java_streams/models/Episodios.java
+++ estudos-java-streams/src/main/java/com/eudes/estudos_java_streams/models/Episodios.java
@@ -1,4 +1,31 @@
 package com.eudes.estudos_java_streams.models;
 
 public class Episodios {
+    private Integer numeroEpisodio;
+    private String tituloEpisodio;
+    private String duracaoMinutosEpisido;
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public String getTituloEpisodio() {
+        return tituloEpisodio;
+    }
+
+    public void setTituloEpisodio(String tituloEpisodio) {
+        this.tituloEpisodio = tituloEpisodio;
+    }
+
+    public String getDuracaoMinutosEpisido() {
+        return duracaoMinutosEpisido;
+    }
+
+    public void setDuracaoMinutosEpisido(String duracaoMinutosEpisido) {
+        this.duracaoMinutosEpisido = duracaoMinutosEpisido;
+    }
 }
