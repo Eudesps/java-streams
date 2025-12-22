98c95d667995d7de0357a1534e2a7ab623b048c1
José Eudes <joseeudes969@gmail.com>
2025-12-22 16:13:16 -0300

    Formatando

--- estudos-java-streams/src/main/java/com/eudes/estudos_java_streams/com/eudes/Teste.java
+++ estudos-java-streams/src/main/java/com/eudes/estudos_java_streams/com/eudes/Teste.java
@@ -2,12 +2,15 @@ package com.eudes.estudos_java_streams.com.eudes;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class Teste {
     public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate hoje = LocalDate.now();
 
         LocalDate aniversarioEudes = LocalDate.of(2002, 8,29);
+        System.out.println("Data de anversáiro: " + aniversarioEudes.format(formatter));
 
         Period periodo = Period.between(aniversarioEudes,hoje);
         System.out.println(periodo.getYears());
