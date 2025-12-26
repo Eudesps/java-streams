0c21436e35cbd2845acb810adc9911b66ccbda97
José Eudes <joseeudes969@gmail.com>
2025-12-26 14:24:32 -0300

    ex8

--- estudos-java-streams/src/main/java/com/eudes/estudos_java_streams/com/eudes/ex007/Ex007.java
+++ estudos-java-streams/src/main/java/com/eudes/estudos_java_streams/com/eudes/ex007/Ex007.java
@@ -6,24 +6,26 @@ package com.eudes.estudos_java_streams.com.eudes.ex007;
 // Filtre todos os produtos da categoria "Eletrônicos" com preço menor que R$ 1000,
 // ordene-os pelo preço em ordem crescente e colete o resultado em uma nova lista.
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Ex007 {
     public static void main(String[] args) {
-        List<Produto> produtos = List.of(
-                new Produto("Notebook", 4500.00, "Eletrônicos"),
-                new Produto("Mouse", 120.00, "Eletrônicos"),
-                new Produto("Cadeira Gamer", 950.00, "Móveis"),
-                new Produto("Livro Java", 180.00, "Educação"),
-                new Produto("Cafeteira", 320.00, "Eletrodomésticos"),
-                new Produto("Fone de Ouvido", 250.00, "Eletrônicos")
+        List<Produto> produtos = Arrays.asList(
+                new Produto("Smartphone", 800.0, "Eletrônicos"),
+                new Produto("Notebook", 1500.0, "Eletrônicos"),
+                new Produto("Teclado", 200.0, "Eletrônicos"),
+                new Produto("Cadeira", 300.0, "Móveis"),
+                new Produto("Monitor", 900.0, "Eletrônicos"),
+                new Produto("Mesa", 700.0, "Móveis")
         );
 
     List<Produto> produtos1 = produtos.stream()
             .filter(p -> p.getCategoria().equals("Eletrônicos"))
             .filter(p -> p.getPreco() < 1000)
             .sorted((p1, p2) -> Double.compare(p1.getPreco(), p2.getPreco()))
+            .limit(3)
             .collect(Collectors.toList());
 
         System.out.println(produtos1);
